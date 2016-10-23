a424ccc9ad9d269bdc36217f42bf97ad46718e4a
bhat0066 <bhat0066@algonquinlive.com>
2016-10-23 19:27:36 -0400

    Add files via upload

--- assignment_one.c
+++ assignment_one.c
@@ -1,11 +1,24 @@
+/* PROGRAM:  assignment_one.c
+   DATE:     23/10/2016
+   TOPIC:    Simple Langauge Simulator
+   PURPOSE:  A small program to simulate the operations of a very simple computer.   
+*/
+
+/**************************************************************************/
+/* Declare include files
+ **************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+/**************************************************************************/
+/* Macro Defines
+ **************************************************************************/
+#define SIZE 1000
+
 /*
  * Operation Codes
  */
-#define SIZE 1000
 #define READ 10
 #define SAVE 11
 
@@ -32,33 +45,46 @@
 #define REG_OPERAND 4
 #define REG_VALID   5
 
-/*
- * Globals
- */
+/**************************************************************************/
+/* Global variables
+ **************************************************************************/
 int words[SIZE];
 int registers[6];
+int maxAddress = 0;
 
-/*
- * Function Definitions
- */
+/**************************************************************************/
+/* Function prototypes
+ **************************************************************************/
 void initialization();
 void load_sml();
 void execute_sml();
 void dump(int status);
 
+/**************************************************************************
+ * Main
+ *    Calls methods initialization(), load_sml(), execute_sml() & dump()
+ *    Returns 0 on success
+ **************************************************************************/
 int main() {
   initialization();
   load_sml();
   execute_sml();
   dump(0);
-
   return 0;
 }
 
+/**************************************************************************
+ * initialization
+ *      Upon execution it initilizes the words array up to the size with 
+ *	50505 & registers array upto 6 with 0's.
+ *      Return: 
+ *           n/a
+ *      Sets global variable words & registers
+ **************************************************************************/
 void initialization() {
   int i;
 
-  for (i = 0; i < 1000; ++i) {
+  for (i = 0; i < SIZE; ++i) {
     words[i] = 50505;
   }
 
@@ -67,6 +93,13 @@ void initialization() {
   }
 }
 
+/**************************************************************************
+ * load_sml
+ *      Upon execution it takes input of word and error checks them while
+	storing valid word in to words array. 
+ *      Return: 
+ *           n/a
+ **************************************************************************/
 void load_sml() {
   int i = 0, word;
 
@@ -76,86 +109,148 @@ void load_sml() {
     if (word == -999999) {
       break;
     }
-
-    words[i++] = word;
+    if(word >= 99999 || word <= -99999){
+    	puts("                      *** ABEND: prg load: prg invalid word ***\n");
+	puts("*********************************ABNORMAL TERMINATION****************************");
+	dump(1);
+	break;	
+    }
+    if(i>SIZE){
+	puts("				*** ABEND: prg load: prg too big ***\n");
+	puts("*********************************ABNORMAL TERMINATION****************************");
+	dump(1);
+	break;
+    }
+    words[i++] = word;   
   }
 }
 
+/**************************************************************************
+ * execute_sml
+ *      Upon execution it takes an word from the words array and splits them
+ *	into Opcode and Operand, depending on valid opcode, it calls upon
+ *	different cases to be executed. If invalid then error message it 
+ * 	put and dump(1); is called upon.
+ *      Return: 
+ *           n/a
+ **************************************************************************/
 void execute_sml() {
-  int done = false;
-
+  int done = false, tempData = 0;
+  puts("*********************************START EXECUTION*********************************");
+  
   while (!done) {
     registers[REG_OPCODE] = words[registers[REG_COUNTER]] / 1000;
     registers[REG_OPERAND] = words[registers[REG_COUNTER]] % 1000;
-
+    
     switch (registers[REG_OPCODE]) {
     case READ:
+      scanf("%d", &tempData);
+      if(tempData > 99999 || tempData < -99999) {
+        puts("                      *** ABEND: prg load: prg invalid word ***\n");
+	puts("*********************************ABNORMAL TERMINATION****************************");
+        dump(1);
+	break;
+      }
+      words[registers[REG_OPERAND]] = tempData;
+      printf("READ:  %+05d\n", words[registers[REG_OPERAND]]); 
+      break;
     case SAVE:
-      scanf("%d", &words[registers[REG_OPERAND]]);
-      if ()
+      registers[REG_ACCUM] = words[registers[REG_OPERAND]];
       break;
     case LOAD:
       registers[REG_ACCUM] = words[registers[REG_OPERAND]];
       break;
+    case STORE:
+      words[registers[REG_OPERAND]] = registers[REG_ACCUM];
+      printf("%+05d\n",  words[registers[REG_OPERAND]]); 
+      break;
     case ADD:
-      registers[REG_ACCUM] += words[registers[REG_OPERAND]];
+      registers[REG_ACCUM] += words[registers[REG_OPERAND]]; 	    
       break;
     case SUBSTRACT:
       registers[REG_ACCUM] -= words[registers[REG_OPERAND]];
       break;
-    case DIVIDE:
+    case DIVIDE:  	
       if (words[registers[REG_OPERAND]] == 0) {
-        puts("*** ABEND: attempted division by 0 ***");
+        puts("				 *** ABEND: attempted division by 0 ***\n");
+        puts("*********************************ABNORMAL TERMINATION****************************");
         dump(1);
+        break;
       }
       registers[REG_ACCUM] /= words[registers[REG_OPERAND]];
       break;
     case MULTIPLY:
       registers[REG_ACCUM] *= words[registers[REG_OPERAND]];
       break;
-    case BRANCH:
-      registers[REG_COUNTER] = registers[REG_OPERAND] - 1;
+    case BRANCH:	
+      registers[REG_COUNTER] = registers[REG_OPERAND];
       break;
     case BRANCHNEG:
-      if (registers[REG_ACCUM] < 0) {
-        registers[REG_COUNTER] = registers[REG_OPERAND] - 1;
+      if (registers[REG_ACCUM] <= 0) {
+        registers[REG_COUNTER] = registers[REG_OPERAND];
       }
       break;
     case BRANCHZERO:
       if (registers[REG_ACCUM] == 0) {
-        registers[REG_COUNTER] = registers[REG_OPERAND] - 1;
+        registers[REG_COUNTER] = registers[REG_OPERAND];
       }
       break;
     case HALT:
-      done = true;
+      puts("*********************************END EXECUTION***********************************");	 
+      dump(1);
       break;
     default:
+      puts("\n             *** ABEND: Addressability error and/or Invalid Opcodeeexit ***\n");
+      puts("*********************************ABNORMAL TERMINATION****************************");
       dump(1);
+      break;
     }
-
+    if(registers[REG_ACCUM] >= 99999){
+        puts("				     *** ABEND: overflow ***\n");
+        puts("*********************************ABNORMAL TERMINATION****************************");
+        dump(1);
+        break;
+    }else if(registers[REG_ACCUM] <= -99999){
+	puts("				     *** ABEND: underflow ***\n");
+        puts("*********************************ABNORMAL TERMINATION****************************");
+        dump(1);        
+	break;
+    } 
     if (!done) {
-      registers[REG_COUNTER]++;
+      registers[REG_COUNTER]++;	
     }
+    
   }
 }
 
+/**************************************************************************
+ * dump
+ *      Prints out essential information of the registers and exits 
+ *	with success or faliure using the status.
+ *      Return: 
+ *           n/a
+ **************************************************************************/
 void dump(const int status) {
   int i;
+ 
+  puts("\nREGISTERS:");
+  printf("accumulator           %+06d\n",    registers[REG_ACCUM]);
+  printf("instructioncounter       %03d\n",  registers[REG_COUNTER]);
+  printf("instructionregister   %+d\n",      words[registers[REG_COUNTER]]);
+  printf("operationcode             %02d\n",   registers[REG_OPCODE]);
+  printf("operand                  %03d\n",  registers[REG_OPERAND]);
+  printf("ValidInstructions         %2d\n\n", ++registers[REG_COUNTER]);
 
-  puts("REGISTERS:");
-  printf("accumulator %d\n", registers[REG_ACCUM]);
-  printf("instructioncounter %d\n", registers[REG_COUNTER]);
-  printf("instructionregister %d\n", registers[REG_INSTR]);
-  printf("operationcode %d\n", registers[REG_OPCODE]);
-  printf("operand %d\n", registers[REG_OPERAND]);
-  printf("ValidInstructions %d\n\n", registers[REG_VALID]);
-
-  printf("MEMORY:\n\n");
-  for (i = 0; i < 20; ++i) {
-    printf("%+d ", words[i]);
+  printf("MEMORY:\n");
+  for (i = 0; i < SIZE; ++i) {
+    if(words[i] == 50505)
+      break;
+    else if(i%10==0){
+      printf("\n%3d ", i);;
+    }
+    printf("%+05d ", words[i]);
+    
   }
-
   puts("");
-
   exit(status);
 }
